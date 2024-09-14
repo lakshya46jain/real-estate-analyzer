f4ad5fddd6cc1d243561c3207a66f2b132a6a0df
adityac2109 <165535469+adityac2109@users.noreply.github.com>
2024-09-14 18:26:10 +0000

    made a web page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+
+
+
+
+export default App;
+// src/App.js
+
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">My App</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Typography variant="h2">Welcome to my app!</Typography>
+      </Container>
     </div>
   );
 }
-
-export default App;
